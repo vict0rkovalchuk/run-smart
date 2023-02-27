d3a2e9e32b773bc87659b55c4ca50a9dbecd31da
Victor <vict02r05@gmail.com>
2023-02-27 14:06:02 +0200

    add modals windows

--- src/js/script.js
+++ src/js/script.js
@@ -47,4 +47,22 @@ $(document).ready(function () {
 
   toggleClasses('catalog-item__back');
   toggleClasses('catalog-item__link');
+
+  $('[data-modal="consultation"]').on('click', function () {
+    $('.overlay, #consultation').fadeIn();
+  });
+
+  $('.modal__close').on('click', function () {
+    $('.overlay, #consultation, #order, #thanks').fadeOut();
+  });
+
+  $('.button_mini').on('click', function () {
+    $('.overlay, #order').fadeIn();
+  });
+
+  $('.button_mini').each(function (i) {
+    $(this).on('click', function () {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+    });
+  });
 });
