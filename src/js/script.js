7d12f4ad0602028f613f1d71d4a4db05cb5f9370
Victor <vict02r05@gmail.com>
2023-03-02 15:43:07 +0200

    add smooth scroll

--- src/js/script.js
+++ src/js/script.js
@@ -119,4 +119,30 @@ $(document).ready(function () {
     });
     return false;
   });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  $('.pageup').on('click', function (event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+
+      const hash = this.hash;
+
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
 });
